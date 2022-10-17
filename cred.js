b01b1a8834a993b1983ab9d3a9ddcff49b392e38
kumar.anand <kumar.anand@kambala.co.in>
2022-10-17 07:00:40 +0530

    updated domain endpoints to api.shoonya.com issue no: 10

--- cred.js
+++ cred.js
@@ -1,8 +1,8 @@
 module.exports.authparams = {
-'userid'   : 'USER',
-'password' : 'Abc123',
-'twoFA'    : '01011970',
-'vendor_code' : 'VC',
-'api_secret' : 'your secret',
+'userid'   : 'FA30417',
+'password' : 'Daiwik@8',
+'twoFA'    : 'AYJPB4562B',
+'vendor_code' : 'FA30417_U',
+'api_secret' : '16592686bb6b07074ed91db0cac6c1a7',
 'imei'       : 'machine id'
 }
